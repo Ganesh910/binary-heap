c25beeb26ff9362418cec1f434328c7e7e1342f0
pandeyganesh <pandeyganesha@gmail.com>
2023-03-04 10:23:49 +0530

    Added heapChecker file that can be used to check the answers returned by the BinaryHeap program

---
+++ heapChecker.py
@@ -0,0 +1,17 @@
+"""This program returns true if the given array is a binary heap, False otherwise."""
+
+class HeapCheck:
+    def __init__(self, arr):
+        self.arr = arr
+
+    def check(self):
+        for i in range((len(self.arr)-2)//2+1):
+            if self.arr[i*2+1]<self.arr[i] or self.arr[i*2+2]<self.arr[i]:
+                return False
+        
+        return True
+
+arr = [2, 3, 5, 9, 1, 7]
+ch = HeapCheck(arr)
+
+print(ch.check())
