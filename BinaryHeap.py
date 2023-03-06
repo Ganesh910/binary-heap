a4adac44b81afdb4520399d8af94a8f96e2b00dd
pandeyganesh <pandeyganesha@gmail.com>
2023-03-06 17:54:59 +0530

    Fixed ht BinaryHeap.py code if last right child also exists and also fixed the same issue for heapChecker.py

--- BinaryHeap.py
+++ BinaryHeap.py
@@ -4,46 +4,55 @@
 class Heap:
     def __init__(self, arr):
         self.arr = arr
-        self.nonLeaf = (len(arr)-2)//2 #index of last non-leaf node, leaf nodes start after that
+        # index of last non-leaf node, leaf nodes start after that
+        self.nonLeaf = (len(arr)-2)//2
         self.build_min_heap()
 
     def build_min_heap(self):
 
-        #Heapifying all non-leaf nodes from last to first
+        # Heapifying all non-leaf nodes from last to first
         for i in range(self.nonLeaf, -1, -1):
             self.heapify(i)
 
-    def heapify(self, index):
-        child1 = index*2+1 #index of left child
-        child2 = index*2+2 #Index of right child
+    def heapify(self, parent):
+
+        if parent > self.nonLeaf:
+            return
+
+        child1 = parent*2+1  # index of left child
+        child2 = parent*2+2  # Index of right child
 
         # Is parent larger than it's left child?
-        if self.arr[index]>self.arr[child1]:
+        if self.arr[parent] > self.arr[child1]:
 
-            # Checks if right child exists or not
-            if child2>=len(self.arr):
-                child2=child1
-            
-            #Is left child smaller than right child?
-            if self.arr[child2]>=self.arr[child1]:
+            # Swaps the parent with the left child if right child does not exist
+            if child2 >= len(self.arr):
+                self.arr[parent], self.arr[child1] = self.arr[child1], self.arr[parent]
+                self.heapify(child1)
+                return
+
+            # Is left child smaller than right child?
+            elif self.arr[child2] < self.arr[child1]:
 
                 # Then Swap them
-                self.arr[index], self.arr[child1]=self.arr[child1], self.arr[index]
+                self.arr[parent], self.arr[child2] = self.arr[child2], self.arr[parent]
 
                 # And then check recusively same thing on the child
-                if child1<=self.nonLeaf:
-                    self.heapify(child1)
-
-            # If right child is even smaller than left, then swap parent with right child
-            else:
-                self.arr[index], self.arr[child2]=self.arr[child2], self.arr[index]
-
-                # Then check recursively same thing on the child
-                if child2<=self.nonLeaf:
+                if child1 <= self.nonLeaf:
                     self.heapify(child2)
+                    return
+            else:
+                self.arr[parent], self.arr[child1] = self.arr[child1], self.arr[parent]
+                self.heapify(child1)
+                return
+                
+        if child2 < len(self.arr) and self.arr[parent] > self.arr[child2]:
+            self.arr[parent], self.arr[child2] = self.arr[child2], self.arr[parent]
+            self.heapify(child2)
+            return
 
     def minimum(self):
-        if len(self.arr)!=0:
+        if len(self.arr) != 0:
             return self.arr[0]
         return None
 
@@ -60,11 +69,13 @@ class Heap:
     def decrease_key(self):
         pass
 
+
 def main():
-    # arr = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7, 2, 4, 5, 7, 8, 4, 3]
-    arr = [5, 3, 7, 9, 1, 2]
+    arr = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7, 2, 4, 5, 7, 8, 4]
+    # arr = [5, 3, 7, 9, 1, 2]
     heap = Heap(arr)
     print(arr)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
--- heapChecker.py
+++ heapChecker.py
@@ -8,12 +8,14 @@ class HeapCheck:
         
         #checking if all the internal nodes are smaller than their children. Returns False if not.
         for i in range((len(self.arr)-2)//2+1):
-            if self.arr[i*2+1]<self.arr[i] or self.arr[i*2+2]<self.arr[i]:
+
+            #if left child is smaller than parent or right child exists and smaller than parent
+            if self.arr[i*2+1]<self.arr[i] or (i*2+2 < len(self.arr) and self.arr[i*2+2]<self.arr[i]):
                 return False
         
         return True
 
-arr = [2, 3, 5, 9, 1, 7]
+arr = [1, 2, 3, 4, 2, 4, 7, 4, 8, 7, 16, 9, 5, 10, 8, 14]
 ch = HeapCheck(arr)
 
 print(ch.check())
